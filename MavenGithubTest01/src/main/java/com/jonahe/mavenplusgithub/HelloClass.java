b9410a688b021e40853ce8164f2edd3e3ac562ba
Erik Wiberg <erik.wiberg.87@gmail.com>
2015-10-14 22:12:42 +0200

    Made class to work with from another project..Testing maven/jitpack.io

---
+++ MavenGithubTest01/src/main/java/com/jonahe/mavenplusgithub/HelloClass.java
@@ -0,0 +1,12 @@
+package com.jonahe.mavenplusgithub;
+
+/**
+ * Use me from another project, using JitPack.io ?
+ *
+ */
+public class HelloClass 
+{
+   public String getHelloMessage(String name){
+	   return "Hello " + name + "!";
+   }
+}
