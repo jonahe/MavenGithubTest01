963e4d158386d9effcccc59638acdeb18150b03b
jonahe <erik.wiberg.87@gmail.com>
2015-10-14 22:40:33 +0200

    New method added from Github

--- MavenGithubTest01/src/main/java/com/jonahe/mavenplusgithub/HelloClass.java
+++ MavenGithubTest01/src/main/java/com/jonahe/mavenplusgithub/HelloClass.java
@@ -9,4 +9,8 @@ public class HelloClass
    public String getHelloMessage(String name){
 	   return "Hello " + name + "!";
    }
+   
+   public String getMessageAddedFromGitHub(){
+   	return "This message was added from github, to ensure that the import really was from here, and not some local variation";
+   }
 }
